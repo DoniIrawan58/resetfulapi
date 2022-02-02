8f5e84bb98d846d7c978b2618262105b5f3daabb
Doni_Irawan <donicyber5@gmail.com>
2022-02-02 13:54:00 +0700

    tersunting

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use App\Models\post;
+use App\Http\Controllers\PostsApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', [PostsApiController::class, 'index']);
+Route::post('/posts', [PostsApiController::class, 'store']);
+Route::post('/posts/{posts}', [PostsApiController::class, 'update']);
+
+Route::delete('/posts/{post}', [PostsApiController::class, 'destroy']);
